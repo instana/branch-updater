892f2707fe35b67472f0db075649453fd4570cec
Jochen Schalanda <jochen.schalanda@instana.com>
2021-10-13 12:21:50 +0200

    Properly handle overriding the baseUrl for PR approval

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ module.exports = ({ app }) => {
   let prApprovalOctokit
   if (process.env.PR_APPROVAL_PERSONAL_ACCESS_TOKEN) {
     prApprovalOctokit = new ProbotOctokit({
+      baseUrl: process.env.GHE_HOST
+        ? `${process.env.GHE_PROTOCOL || "https"}://${process.env.GHE_HOST}/api/v3`
+        : "https://api.github.com",
       auth: {
         token: process.env.PR_APPROVAL_PERSONAL_ACCESS_TOKEN
       }
