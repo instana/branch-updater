e30da5de29249ae2e459b6644615559b10f57c6f
Reinhard Nägele <reinhard.naegele@ibm.com>
2025-06-06 10:59:59 +0200

    Fix build and dependencies

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ async function onPush (context, prApprovalOctokit) {
   }
 
   if (!isReleaseBranch(branch)) {
-    context.log.info(`Branch is not one of the potential change sources.`)
+    context.log.info('Branch is not one of the potential change sources.')
     return
   }
 
@@ -93,7 +93,7 @@ async function onPush (context, prApprovalOctokit) {
     res => res.data.map(branch => branch.name)
   )
 
-  let releaseBranches = allBranches
+  const releaseBranches = allBranches
     .filter(isReleaseBranch)
     .sort((a, b) => a.localeCompare(b))
 
@@ -110,7 +110,7 @@ async function onPush (context, prApprovalOctokit) {
     }
   }
 
-  let pull = await findPullRequest(context)
+  const pull = await findPullRequest(context)
   if (!pull) {
     const response = await createPull(base, branch, context)
     if (response && response.status === 201) {
